6330c25f6d61e9afa659899c4f073ed1f3c8cf94
Cat Hanbit <dudtlr102@naver.com>
2021-06-16 22:35:42 +0900

    this 사용

---
+++ src/step01/Person.java
@@ -0,0 +1,11 @@
+package step01;
+
+class Person {
+    String name;
+    int age;
+    public Person(String name, int age) {
+        // 매개변수로 받은 name과 age를 각각 name, age 필드에 저장하세요.
+        this.name = name;
+        this.age = age;
+    }
+}

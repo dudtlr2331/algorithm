29f2cd5aa4a961568df64e191b10209f5705a77a
Cat Hanbit <dudtlr102@naver.com>
2021-06-17 23:55:09 +0900

    생성자 오버로딩과 this

--- src/step01/Car.java
+++ src/step01/Car.java
@@ -1,12 +1,30 @@
 package step01;
 
-class Car {
-    void run() {
-        System.out.println("차가 달립니다.");        
-    }
-    // 정수 하나를 매개변수로 받는 메소드, run을 추가해 보세요.
-    void run(int a){
-        System.out.println("차가 " + a +"대 달립니다.");
+//class Car {
+//    void run() {
+//        System.out.println("차가 달립니다.");        
+//    }
+//    // 정수 하나를 매개변수로 받는 메소드, run을 추가해 보세요.
+//    void run(int a){
+//        System.out.println("차가 " + a +"대 달립니다.");
+//    }
+//    
+//}
+
+public class Car {
+    String name;
+    int number;
+
+    Car(){
+        this("이름없음", 0);
     }
     
+    Car(String name){
+        this(name, 0);
+    }
+    
+    Car(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
 }

3bfc6c16b670c84f2b3b2b79ffb4552e35b0bc18
Cat Hanbit <dudtlr102@naver.com>
2021-06-17 23:50:33 +0900

    메소드 오버로딩

---
+++ src/step01/Car.java
@@ -0,0 +1,12 @@
+package step01;
+
+class Car {
+    void run() {
+        System.out.println("차가 달립니다.");        
+    }
+    // 정수 하나를 매개변수로 받는 메소드, run을 추가해 보세요.
+    void run(int a){
+        System.out.println("차가 " + a +"대 달립니다.");
+    }
+    
+}

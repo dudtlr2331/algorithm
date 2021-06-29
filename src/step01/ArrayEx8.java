c8a31131755d6e226fdb1b628b54a6343eda4406
Cat Hanbit <dudtlr102@naver.com>
2021-06-29 20:02:55 +0900

    배열 연습 문제

---
+++ src/step01/ArrayEx8.java
@@ -0,0 +1,12 @@
+package step01;
+
+public class ArrayEx8 {
+
+	public static void main(String[] args) {
+		System.out.println("매개변수의 개수 : " + args.length);
+		for(int i = 0; i < args.length; i++) {
+			System.out.println("args[" + i + "] = \""+ args[i] + "\"");
+		}
+	}
+
+}

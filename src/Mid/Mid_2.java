901c64d5cb192b7a3d297e9299b8f4b6cc40d03b
Cat Hanbit <dudtlr102@naver.com>
2021-07-03 13:13:28 +0900

    1학기 중간

---
+++ src/Mid/Mid_2.java
@@ -0,0 +1,23 @@
+package Mid;
+
+public class Mid_2 {
+	String name;
+	String address;
+	int studentNum;
+	
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public int getStudentNum() {
+		return studentNum;
+	}
+
+	public Mid_2(String name, String address, int studentNum) {
+		this.name = name;
+		this.address = address;
+		this.studentNum = studentNum;
+	}
+}
